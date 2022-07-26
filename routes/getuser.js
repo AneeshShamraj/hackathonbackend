ee6a2835f8a6bf4b0d46364cbe86dcb29395ecbd
Aneesh Shamraj <aneesh.shamraj@gmail.com>
2022-07-27 00:00:17 +0530

    Make teacher route

--- routes/getuser.js
+++ routes/getuser.js
@@ -3,19 +3,37 @@ import User from "../models/user.js";
 import validate from "../models/user.js";
 import bcrypt from "bcrypt";
 
-const userData = express.Router();
+const router = express.Router();
 
-userData.get("/",async(req,res) =>
+router.post("/acceptrequest",async(req,res) =>
 {
-        try {
-          const getuserdata = await User.find();
-          res.send(getuserdata);
-        } catch (e) {
-                res.status(409).send({ message:"xyz"})
+        const objid=req.body.objid;
+        const currentevent=await StudentEvents.findOne({ _id: objid });
+        const currentdate=currentevent.date;
+        await StudentEvents.deleteMany({ date: currentdate });
+        res.json(await StudentEvents.updateOne({_id:objid},{approved:true}));
+
+
+});
+
+router.post("/denyrequest",async(req,res) =>
+{
+        const objid=req.body.objid;
+        try{
+                res.send(await StudentEvents.deleteOne({ _id:objid }));
         }
+        catch{
+                res.status(400).send("Error");
+        }
+        
+
+});
+
+router.get("/getpendingrequests",async(req,res)=>
+{
+        var foundrequests=await StudentEvents.find({approved:false});
+        res.json(foundrequests);
+});
 
 
-})
-
-
-export default userData;
+export default router;
